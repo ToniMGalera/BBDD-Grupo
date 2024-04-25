38d030dbf107b55f63aa39d1c0cbcf07345c2e7a
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-04-25 11:59:36 +0200

    mejoras

--- MasonesCompany.sql
+++ MasonesCompany.sql
@@ -1,16 +1,17 @@
 CREATE DATABASE masonescompany;
 USE masonescompany;
 
---proveedores
+-- Proveedores
 CREATE TABLE Proveedores (
     id_proveedor INT AUTO_INCREMENT, 
     nombre NVARCHAR(100) NOT NULL,
     correo NVARCHAR (100) UNIQUE NOT NULL,
     telefono VARCHAR (14),
     direccion NVARCHAR (255),
-    PRIMARY KEY (id_proveedor));
+    PRIMARY KEY (id_proveedor)
+);
 
---tabla productos
+-- Productos
 CREATE TABLE Productos (
   id_producto INT AUTO_INCREMENT,
   nombre NVARCHAR(100) NOT NULL,
@@ -18,11 +19,12 @@ CREATE TABLE Productos (
   foto VARCHAR (255),
   precio DECIMAL(10,2) NOT NULL,
   stock INT NOT NULL,
-  proveedor CHAR (5),
+  proveedor INT,
   PRIMARY KEY (id_producto),
-  FOREIGN KEY (proveedor) REFERENCES Proveedores(id_proveedor) ON DELETE CASCADE ON UPDATE CASCADE);
+  FOREIGN KEY (proveedor) REFERENCES Proveedores(id_proveedor) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
---clientes
+-- Clientes
 CREATE TABLE Clientes (
   id_cliente INT AUTO_INCREMENT,
   nombre NVARCHAR(100) NOT NULL,
@@ -32,30 +34,33 @@ CREATE TABLE Clientes (
   correo VARCHAR(100) UNIQUE NOT NULL,
   telefono VARCHAR(14) NOT NULL,
   responsable NVARCHAR (100),
-  PRIMARY KEY (id_cliente));
+  PRIMARY KEY (id_cliente)
+);
 
---pedidos
+-- Pedidos
 CREATE TABLE Pedidos (
   id_pedido INT AUTO_INCREMENT,
   fecha_pedido DATE,
   total_pedido DECIMAL(10,2),
-  estado_pedido ENUM('pendiente', 'acceptado', 'en_preparación', 'enviado', 'entregado', 'cancelado'),
+  estado_pedido ENUM('pendiente', 'aceptado', 'en_preparación', 'enviado', 'entregado', 'cancelado'),
   cliente INT,
   PRIMARY KEY (id_pedido),
-  FOREIGN KEY (cliente) REFERENCES Clientes(id_cliente));
+  FOREIGN KEY (cliente) REFERENCES Clientes(id_cliente)
+);
 
+-- Devoluciones
 CREATE TABLE Devoluciones (
   id_devolucion INT AUTO_INCREMENT,
   fecha_devolucion DATE,
-  estado_devolucion ENUM('pendiente', 'enviado', 'por_recibir', 'entregado', 'cancelado', 'en_revision', 'acceptado'),
+  estado_devolucion ENUM('pendiente', 'enviado', 'por_recibir', 'entregado', 'cancelado', 'en_revision', 'aceptado'),
   cliente INT,
-  pedido,
+  pedido INT,
   PRIMARY KEY (id_devolucion),
-  FOREIGN KEY (cliente) REFERENCES Clientes(id_cliente) ON DELETE CASCADE ON UPDATE CASCADE),
-  FOREIGN KEY (pedido) REFERENCES Pedidos(id_pedido) ON DELETE CASCADE ON UPDATE CASCADE;
+  FOREIGN KEY (cliente) REFERENCES Clientes(id_cliente) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (pedido) REFERENCES Pedidos(id_pedido) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-
---detalles pedidos
+-- Detalles pedidos
 CREATE TABLE DetallesPedidos (
   id_detalle_pedido INT AUTO_INCREMENT,
   pedido_id INT,
@@ -64,8 +69,9 @@ CREATE TABLE DetallesPedidos (
   precio_unitario DECIMAL(10,2),
   subtotal DECIMAL(10,2),
   PRIMARY KEY (id_detalle_pedido),
-  FOREIGN KEY (pedido_id) REFERENCES Pedidos(id_pedido) ON DELETE CASCADE ON UPDATE CASCADE),
-  FOREIGN KEY (producto_id) REFERENCES Productos(id_producto);
+  FOREIGN KEY (pedido_id) REFERENCES Pedidos(id_pedido) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (producto_id) REFERENCES Productos(id_producto) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 INSERT INTO proveedores (nombre, correo, telefono, direccion)
 VALUES
@@ -80,47 +86,47 @@ VALUES
   ( 'Toshiba Corporation', 'toshiba@example.com', '777777777', 'Calle 9, 678'),
   ( 'Lenovo Group', 'lenovo@example.com', '888888888', 'Calle 10, 901');
 
-INSERT INTO Clientes ( nombre, apellido_1, apellido_2, dirección, correo, telefono)
+INSERT INTO Clientes (nombre, apellido_1, apellido_2, dirección, correo, telefono, responsable)
 VALUES
-  ('Juan', 'Pérez', 'González', 'Calle 1, 123', 'juan.perez@example.com', '123456789'),
-  ('María', 'García', 'Hernández', 'Calle 2, 456', 'maria.garcia@example.com', '987654321'),
-  ('Pedro', 'Martínez', 'Rodríguez', 'Calle 3, 789', 'pedro.martinez@example.com', '111111111'),
-  ('Ana', 'Sánchez', 'Gómez', 'Calle 4, 901', 'ana.sanchez@example.com', '222222222'),
-  ('Luis', 'García', 'Hernández', 'Calle 5, 234', 'luis.garcia@example.com', '333333333'),
-  ('Eva', 'Hernández', 'Gómez', 'Calle 6, 567', 'eva.hernandez@example.com', '444444444'),
-  ('Carlos', 'Rodríguez', 'García', 'Calle 7, 890', 'carlos.rodriguez@example.com', '555555555'),
-  ('Sofía', 'Gómez', 'Hernández', 'Calle 8, 345', 'sofia.gomez@example.com', '666666666'),
-  ('Lucía', 'Fernández', 'García', 'Calle 9, 678', 'lucia.fernandez@example.com', '777777777'),
-  ('Jorge', 'López', 'Gómez', 'Calle 10, 901', 'jorge.lopez@example.com', '888888888');
+  ('Juan', 'Pérez', 'González', 'Calle 1, 123', 'juan.perez@example.com', '123456789', 'Toni'),
+  ('María', 'García', 'Hernández', 'Calle 2, 456', 'maria.garcia@example.com', '987654321', 'Toni'),
+  ('Pedro', 'Martínez', 'Rodríguez', 'Calle 3, 789', 'pedro.martinez@example.com', '111111111', 'Toni'),
+  ('Ana', 'Sánchez', 'Gómez', 'Calle 4, 901', 'ana.sanchez@example.com', '222222222', 'Toni'),
+  ('Luis', 'García', 'Hernández', 'Calle 5, 234', 'luis.garcia@example.com', '333333333', 'Toni'),
+  ('Eva', 'Hernández', 'Gómez', 'Calle 6, 567', 'eva.hernandez@example.com', '444444444', 'Toni'),
+  ('Carlos', 'Rodríguez', 'García', 'Calle 7, 890', 'carlos.rodriguez@example.com', '555555555', 'Toni'),
+  ('Sofía', 'Gómez', 'Hernández', 'Calle 8, 345', 'sofia.gomez@example.com', '666666666', 'Toni'),
+  ('Lucía', 'Fernández', 'García', 'Calle 9, 678', 'lucia.fernandez@example.com', '777777777', 'Toni'),
+  ('Jorge', 'López', 'Gómez', 'Calle 10, 901', 'jorge.lopez@example.com', '888888888', 'Toni');
 
 
-INSERT INTO Productos (nombre, descripción, precio, stock)
+INSERT INTO Productos (nombre, descripción, precio, stock, proveedor)
 VALUES
-  ('Televisor', 'Televisor 4K', 500.00, 10),
-  ('Laptop', 'Laptop de 15 pulgadas', 800.50, 5),
-  ('Tablet', 'Tablet con pantalla táctil', 300.25, 12),
-  ('Smartphone', 'Smartphone con cámara de 12 MP', 600.75, 8),
-  ('Consola de videojuegos', 'Consola de videojuegos con juego incluido', 250.99, 15),
-  ('Impresora 3D', 'Impresora 3D para imprimir objetos', 1200.50, 3),
-  ('Cámara de fotos', 'Cámara de fotos con estabilizador óptico', 400.25, 10),
-  ('Reproductor de música', 'Reproductor de música con almacenamiento de 64 GB', 150.50, 12),
-  ('Gafas de realidad virtual', 'Gafas de realidad virtual con pantalla de 5 pulgadas', 200.75, 8),
-  ('Tableta para niños', 'Tableta para niños con juegos educativos', 180.99, 15);
+  ('Televisor', 'Televisor 4K', 500.00, 10, 1),
+  ('Laptop', 'Laptop de 15 pulgadas', 800.50, 5, 2),
+  ('Tablet', 'Tablet con pantalla táctil', 300.25, 12, 2),
+  ('Smartphone', 'Smartphone con cámara de 12 MP', 600.75, 8, 1),
+  ('Consola de videojuegos', 'Consola de videojuegos con juego incluido', 250.99, 15, 3),
+  ('Impresora 3D', 'Impresora 3D para imprimir objetos', 1200.50, 3, 4),
+  ('Cámara de fotos', 'Cámara de fotos con estabilizador óptico', 400.25, 10, 5),
+  ('Reproductor de música', 'Reproductor de música con almacenamiento de 64 GB', 150.50, 12, 6),
+  ('Gafas de realidad virtual', 'Gafas de realidad virtual con pantalla de 5 pulgadas', 200.75, 8, 7),
+  ('Tableta para niños', 'Tableta para niños con juegos educativos', 180.99, 15, 8);
 
-INSERT INTO Pedidos ( fecha_pedido, total_pedido)
+INSERT INTO Pedidos (fecha_pedido, total_pedido, estado_pedido, cliente)
 VALUES
-  ('2024-01-01', 1000.00),
-  ('2024-02-01', 800.50),
-  ('2024-03-01', 600.75),
-  ('2024-04-01', 250.99),
-  ('2024-05-01', 1200.50),
-  ('2024-06-01', 400.25),
-  ('2024-07-01', 150.50),
-  ('2024-08-01', 200.75),
-  ('2024-09-01', 180.99),
-  ('2024-10-01', 250.00);
+  ('2024-01-01', 1000.00, 'pendiente', 1),
+  ('2024-02-01', 800.50, 'aceptado', 2),
+  ('2024-03-01', 600.75, 'en_preparación', 3),
+  ('2024-04-01', 250.99, 'cancelado', 4),
+  ('2024-05-01', 1200.50, 'entregado', 5),
+  ('2024-06-01', 400.25, 'enviado', 5),
+  ('2024-07-01', 150.50, 'enviado', 2),
+  ('2024-08-01', 200.75, 'cancelado', 4),
+  ('2024-09-01', 180.99, 'entregado', 8),
+  ('2024-10-01', 250.00, 'aceptado', 9);
 
-INSERT INTO Devoluciones ( fecha_devolucion, estado_devolucion, cliente, pedido)
+INSERT INTO Devoluciones (fecha_devolucion, estado_devolucion, cliente, pedido)
 VALUES
   ('2024-01-01', 'Rechazado', 1, 1),
   ('2024-02-01', 'Aceptado', 2, 2),
